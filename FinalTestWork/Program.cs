332a35fdc5a3f1efc15e2bd049368e33088b61b2
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-09-20 18:44:12 +0300

    Добавил код

--- FinalTestWork/Program.cs
+++ FinalTestWork/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FinalTestWork
 {
@@ -9,6 +6,20 @@ namespace FinalTestWork
     {
         static void Main(string[] args)
         {
-        }
+			string[] array = { "hello", "2", "world", "jerk", "an" };
+			string[] result = { };
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i].Length <= 3)
+				{
+					Array.Resize(ref result, result.Length + 1);
+					result[result.Length - 1] = array[i];
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", result));
+			Console.ReadLine();
+		}
     }
 }
